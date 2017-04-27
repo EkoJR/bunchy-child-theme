989e56f59bc5fd3033a609c4a772828d671bdd28
EkoJr <ekojr1337@gmail.com>
2017-04-26 22:49:22 -0700

    Added Template Files for Taxonomies/Archive

---
+++ template-parts/archive-three-featured-classic-sidebar.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * The Template for displaying archive body.
+ *
+ * @license For the full license information, please view the Licensing folder
+ * that was distributed with this source code.
+ *
+ * @package Bunchy_Theme
+ */
+
+// Prevent direct script access.
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'No direct script access allowed' );
+}
+?>
+<?php 
+global $wp_query;
+$temp_query = $wp_query;
+
+$bunchy_template_data = bunchy_get_template_part_data();
+
+?>
+
+
+
+<?php
+if ( bunchy_show_archive_featured_entries() ) :
+	get_template_part( 'template-parts/featured-content-3' );
+endif;
+?>
+<?php if ( have_posts() ) : ?>
+	<div class="g1-row g1-row-layout-page archive-body">
+		<div class="g1-row-inner">
+			<div id="primary" class="g1-column">
+				<!-- DO STUFF -->
+				<div class="bs-collection-01">
+					
+				</div>
+				<div class="bs-collection-02">
+					
+				</div>
+				
+				<div class="bc-collection-end">
+					<?php $bunchy_post_number = 0; ?>
+					<?php while ( have_posts() ) : the_post();
+						$bunchy_post_number ++; ?>
+						<!-- Add final template -->
+						<?php get_template_part( 'template-parts/content', 'bs-collection-end' ); ?>
+					<?php endwhile; ?>
+				</div>
+				<div class="bs-collection-pagination">
+					<?php get_template_part( 'template-parts/archive-pagination', $bunchy_template_data['pagination'] ); ?>
+				</div>
+				<!-- END STUFF -->
+			</div><!-- .g1-column -->
+		</div><!-- .g1-row-inner -->
+		<div class="g1-row-background"></div>
+	</div><!-- .g1-row -->
+<?php else : ?>
+	<?php get_template_part( 'template-parts/archive-no-results' ); ?>
+<?php endif;
